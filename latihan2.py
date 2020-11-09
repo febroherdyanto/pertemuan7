d6d77bb59a4333af3ab719edbc2791d6457f4bea
Febro Herdyanto <febroherdyanto@outlook.com>
2020-11-09 19:58:26 +0700

    Update Latihan 1

--- latihan2.py
+++ latihan2.py
@@ -4,3 +4,11 @@ import random
 
 for x in list(range(1,n+1,1)):
     print(random.uniform(0,0.5))
+
+print()
+
+print("===========================")
+print("= Nama  : Febro Herdyanto =")
+print("= NIM   : 312010043       =")
+print("= Kelas : TI.20.B.1       =")
+print("===========================")
